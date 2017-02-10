36aef74791ebbafd3b26d611fe978d02e516c7c0
carsonkk <carsonkk@gmail.com>
2017-02-10 03:33:14 -0800

    non-deprecated ip translation functions, server socket + connect

--- smptcp.c
+++ smptcp.c
@@ -3,24 +3,33 @@
 int main(int argc, char *argv[])
 {
 	opterr = 0;
-	int32_t option_index = 0;
 	int32_t ret = 0;
+	int32_t status;
 	int32_t c;
-	int32_t num_interfaces_i;
-	int32_t port_i;
+	int32_t serv_sock_fd;
+	int32_t num_interfaces;
+	int32_t port;
 	long num_interfaces_l;
 	long port_l;
-	err_msg = "internal error: ";
+	err_m = "internal error: ";
 	char *n_value = NULL;
 	char *h_value = NULL;
 	char *p_value = NULL;
 	char *f_value = NULL;
 	char *usage = "invalid or missing options\nusage: ./mptcp [-n num_interfaces] [-h hostname] [-p port] [-f filename]\n";
 	char *end;
-	struct addrinfo unqualified;
-	struct addrinfo *addr_grp;
-	struct addrinfo *addr;
-	struct sockaddr_in *addr_in;
+	char hostname[INET_ADDRSTRLEN];
+	char *filename;
+	struct addrinfo hints;
+	struct addrinfo *serv_info;
+	struct addrinfo *s;
+	struct sockaddr_in *serv_addr_name = NULL;
+	struct sockaddr_in serv_addr;
+	socklen_t serv_addr_len;
+
+	/*
+		parse through user input
+	*/
 
 	//begin command-line parsing
 	while(1)
@@ -36,12 +45,14 @@ int main(int argc, char *argv[])
 			};
 
 		//initialize the index and c
-		option_index = 0;
+		int option_index = 0;
 		c = getopt_long(argc, argv, "n:h:p:f:", long_options, &option_index);
 
 		//make sure the end hadn't been reached
 		if(c == -1)
+		{
 			break;
+		}
 
 		//cycle through the arguments
 		switch(c)
@@ -80,7 +91,7 @@ int main(int argc, char *argv[])
 	}
 
 	//post-parsing error handling
-	if(ret)
+	if(ret || n_value == NULL || h_value == NULL || p_value == NULL || f_value == NULL)
 	{
 		fprintf(stdout, "%s", usage);
 		return 1;
@@ -94,15 +105,15 @@ int main(int argc, char *argv[])
 		fprintf(stdout, "%s", usage);
 		return 1;
 	}
-	num_interfaces_i = (int32_t)num_interfaces_l;
+	num_interfaces = (int32_t)num_interfaces_l;
 
 	//setup unqulaified for searching for the hostname's mapping
-	memset(&unqualified, 0, sizeof(unqualified));
-	unqualified.ai_family = AF_UNSPEC;
-	unqualified.ai_socktype = SOCK_STREAM;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
 
 	//get the address info with the unqualified hostname
-	ret = getaddrinfo(h_value, "http", &unqualified, &addr_grp);
+	ret = getaddrinfo(h_value, "http", &hints, &serv_info);
 	if(ret)
 	{
 		fprintf(stdout, "%s", usage);
@@ -110,11 +121,22 @@ int main(int argc, char *argv[])
 	}
 
 	//sort through the address info
-	for(addr = addr_grp; addr != NULL; addr = addr->ai_next)
+	for(s = serv_info; s != NULL; s = s->ai_next)
 	{
-		addr_in = (struct sockaddr_in *)addr->ai_addr;
+		//ignore trailing null ip of common public-facing hostnames
+		if(strcmp(inet_ntop(AF_INET, &(((struct sockaddr_in *)s->ai_addr)->sin_addr), hostname, INET_ADDRSTRLEN), "0.0.0.0") == 0)
+		{
+			break;
+		}
+		serv_addr_name = (struct sockaddr_in *)s->ai_addr;
 	}
-	freeaddrinfo(addr_grp);
+	if(serv_addr_name == NULL)
+	{
+		fprintf(stdout, "%s", usage);
+		return 1;
+	}
+	inet_ntop(AF_INET, &(serv_addr_name->sin_addr), hostname, INET_ADDRSTRLEN);
+	freeaddrinfo(serv_info);
 
 	//make sure the port is specified correctly/within range
 	errno = 0;
@@ -124,7 +146,7 @@ int main(int argc, char *argv[])
 		fprintf(stdout, "%s", usage);
 		return 1;
 	}
-	port_i = (int32_t)port_l;
+	port = (int32_t)port_l;
 
 	//make sure the filename is valid and exists
 	ret = access(f_value, F_OK|R_OK);
@@ -133,14 +155,68 @@ int main(int argc, char *argv[])
 		fprintf(stdout, "%s", usage);
 		return 1;
 	}
+	filename = f_value;
 
-	//esatblish initial connection with server
-	//request num_interfaces from server
-	//recv num_interfaces port number back
-	//open threads for each port number
-	//detect file transmission finished
-	//resync threads, check for any errors
-	//finish
+	/*
+		esatblish initial connection with server
+	*/
+
+	//setup server sockaddr_in object
+	serv_addr_len = sizeof(serv_addr);
+	memset((char *)&serv_addr, 0, serv_addr_len);
+	serv_addr.sin_family = AF_INET;
+	inet_pton(AF_INET, hostname, &(serv_addr.sin_addr));
+	serv_addr.sin_port = htons(port);
+
+	//open a client socket
+	status = mp_socket(AF_INET, SOCK_MPTCP, IPPROTO_TCP);
+	if(status < 0)
+	{
+		fprintf(stderr, "%s failed to open an intial server socket (errno[%d]: %s)\n", 
+				err_m, errno, strerror(errno));
+		return 1;
+	}
+	serv_sock_fd = status;
+
+	//connect to the server
+	status = mp_connect(serv_sock_fd, (struct sockaddr *)&serv_addr, serv_addr_len);
+	if(status < 0)
+	{
+		fprintf(stderr, "%s failed to establish intial connection with the server (errno[%d]: %s)\n", 
+				err_m, errno, strerror(errno));
+		return 1;
+	}
+
+	/*
+		send request for num_interface ports to server
+	*/
+
+
+
+
+	/*
+		recv response for num_interface ports from server
+	*/
+
+
+	/*
+		open threads for each port number
+	*/
+
+
+	/*
+		detect file transmission completion
+	*/
+
+
+	/*
+		resync threads, check for any errors
+	*/
+
+
+	/*
+		finish
+	*/
 
 	return ret;
 }
--- threads.h
+++ threads.h
@@ -16,14 +16,21 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <pthread.h>
+#include "mptcp.h"
 
 /*structs*/
 
 /*variables*/
-char *err_msg;
+char *err_m;
 
 /*functions*/
 void *send_thread(void *arg);
 void *recv_thread(void *arg);
 
 #endif
+
+/*	fprintf(stdout, "n_val: %d\n", num_interfaces);
+	fprintf(stdout, "h_val: %s\n", hostname);
+	fprintf(stdout, "p_val: %d\n", port);
+	fprintf(stdout, "f_val: %s\n", filename);
+*/
